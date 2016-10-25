5cb99fadcd9ecca64d50daaa5b2a384ad3eace88
jb.squee <jb.squee@daum.net>
2016-10-25 21:01:59 +0900

    git ignore appled

--- config/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports =  {
-    'facebookAuth' : {
-        'clientID'      : '1706266883028516',
-        'clientSecret'  : '',
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
-        'profileFields'	: ['id', 'displayName', 'picture']
-    }
-};
