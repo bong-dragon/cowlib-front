41f92a7e3196355a7cf1038693a90b420394232a
이경륜 <erin314@naver.com>
2016-11-13 15:10:42 +0900

    [#22] OwnerBook.js 수정 및 console 오류 수정

--- src/section/library/OwnerBook.js
+++ src/section/library/OwnerBook.js
@@ -4,14 +4,16 @@ export default class OwnerBook extends React.Component {
     render () {
     	var book = this.props.book;
     	var wait_list = book.wait_list.map(function (waiter, i) {
-    		return (<p><span>{waiter.name}</span><img className="profile" src={waiter.profile} /></p>)
+    		return (<li><span>{waiter.name}</span><img className="profile" src={waiter.profile} /></li>)
     	})
         return (<li>
-                    <img className="book_img" src={book.cover_l_url} alt={book.title}/>
+                    <div className="book_img">
+                        <img className="book_img" src={book.cover_l_url} alt={book.title} />                    
+                    </div>
                     <div className="info">
                         <p>제목 : {book.title}</p>
                         <p>빌려간사람 : {book.borrow.name? book.borrow.name:no_borrow}</p>
-                        <p>대기자 : {wait_list}</p>
+                        <ul>대기자 : {wait_list}</ul>
                     </div>
                 </li>)
     }
