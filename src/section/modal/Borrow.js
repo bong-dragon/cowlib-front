1269e497b99626741f89d5fd8d8932c66e96ba04
jbsquee <jb.squee@daum.net>
2016-12-05 19:27:14 +0900

    refactoring return/borrow

--- src/section/modal/Borrow.js
+++ src/section/modal/Borrow.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
 import 'whatwg-fetch';
 import {Link} from 'react-router';
-import {parseJson, handleError} from '../../support/Ajax'
-import {borrowBook} from '../../action';
+import {connect} from 'react-redux';
 import {_} from 'underscore'
 
-import {connect} from 'react-redux';
+import {parseJson, handleError} from '../../support/Ajax'
+import {borrowBook} from '../../action';
+
 
 class Borrow extends React.Component {
 
@@ -17,9 +19,9 @@ class Borrow extends React.Component {
     }
 
     borrowBook() {
-        let callNumberId  = this.props.params.callNumberId;
+        let callNumberId = this.props.params.callNumberId;
         let borrowerId = this.props.params.reserverId;
-        console.log(borrowerId);
+
         let url = `/v1/callNumbers/${callNumberId}/borrow?borrowerId=${borrowerId}`;
 
         fetch(url, {
@@ -37,81 +39,81 @@ class Borrow extends React.Component {
     }
 
     returnBook() {
-        let callNumberId  = this.props.params.callNumberId;
+        let callNumberId = this.props.params.callNumberId;
         let borrowerId = this.props.params.reserverId;
         console.log("빌려줌 취소")
     }
 
     render() {
-        console.log(this.state.status);
-        let callNumberId  = this.props.params.callNumberId;
+        let callNumberId = this.props.params.callNumberId;
         let reserverId = this.props.params.reserverId;
         let books = this.props.books;
 
         if (books.length == 0) {
             if (this.props.userId) {
-                let pathname = "/"+this.props.userId;
+                let pathname = "/" + this.props.userId;
                 // 나중에 로딩중 표시 하고, 서버 갔다오면 좋을듯!
                 return (<section>
-                        <p>잘못된 경로로 접근하셨습니다</p>
-                        <Link to={pathname}>내 도서관으로</Link></section>);
-            }else {
+                    <p>잘못된 경로로 접근하셨습니다</p>
+                    <Link to={pathname}>내 도서관으로</Link></section>);
+            } else {
                 return (<section>
-                        <p>잘못된 경로로 접근하셨습니다</p>
-                        <Link to="/">홈으로</Link></section>);
+                    <p>잘못된 경로로 접근하셨습니다</p>
+                    <Link to="/">홈으로</Link></section>);
             }
         }
 
-        let book = _.find(books, function (one) {
-                    return one.callNumber && one.callNumber.id == callNumberId
-                });
+        let book = _.find(books, function (book) {
+            return book.callNumber && book.callNumber.id == callNumberId
+        });
         let bookMeta = book.bookMeta;
         let title = bookMeta.title.replace(/&lt;/g, "").replace(/&gt;/g, "");
-        let book_img = bookMeta.coverUrl ? bookMeta.coverUrl : '/img/basic_book.png';
-        let result;
+        let bookImage = bookMeta.coverUrl ? bookMeta.coverUrl : '/img/basic_book.png';
 
-        if (this.state.status == "BEFORE_BORROW") {
-            let reserver = _.find(book.reservers, function (one) {
-                return one.id && one.id == reserverId
-            });
-
-            result = (<div>
-                        <p className="messageContainer">
-                            <img className="profile" src={reserver.profile} alt="profile"/>
-                            <span>{reserver.name}</span>
-                            <span> 에게 빌려주실 건가요?</span>
-                        </p>
-                        <p className="selectContainer">
-                            <button className="button button_small">아니요</button>
-                            <button className="button button_small" onClick={this.borrowBook.bind(this)}>네</button>
-                        </p>
-                    </div>);
-        }else {
-            let myLibrary = "/"+this.props.userId;
-            result = (<div>
-                        <p className="messageContainer">
-                           <span>빌려줌 상태로 바뀌었습니다.</span>
-                        </p>
-                        <p className="selectContainer">
-                            <button className="button button_small" onClick={this.returnBook.bind(this)}>빌려줌 취소</button>
-                            <button className="button button_small"><Link to={myLibrary}>내도서관</Link></button>
-                        </p>
-                    </div>)
-        }
-
+        let actionWrapper = this.createActionWrapper(book, reserverId);
 
         return (<section className="borrowContainer">
-                    <div className="bookContainer">
-                        <div className="book_img book_img_samll">
-                            <img className="book_img book_img_samll" src={book_img} alt={bookMeta.coverUrl}/>
-                        </div>
-                        <div className="info">
-                            <p dangerouslySetInnerHTML={{__html: title}}/>
-                            <p><span>{bookMeta.author}</span> | <span>{bookMeta.publisher}</span></p>
-                        </div>
-                    </div>
-                    {result}
-                </section>);
+            <div className="bookContainer">
+                <div className="book_img book_img_samll">
+                    <img className="book_img book_img_samll" src={bookImage} alt={bookMeta.coverUrl}/>
+                </div>
+                <div className="info">
+                    <p dangerouslySetInnerHTML={{__html: title}}/>
+                    <p><span>{bookMeta.author}</span> | <span>{bookMeta.publisher}</span></p>
+                </div>
+            </div>
+            {actionWrapper}
+        </section>);
+    }
+
+    createActionWrapper(book, reserverId) {
+        if (this.state.status === "BEFORE_BORROW") {
+            let reserver = _.find(book.reservers, function (reserver) {
+                return reserver.id && reserver.id == reserverId
+            });
+            return (<div>
+                <p className="messageContainer">
+                    <img className="profile" src={reserver.profile} alt="profile"/>
+                    <span>{reserver.name}</span>
+                    <span> 에게 빌려주실 건가요?</span>
+                </p>
+                <p className="selectContainer">
+                    <button className="button button_small">아니요</button>
+                    <button className="button button_small" onClick={this.borrowBook.bind(this)}>네</button>
+                </p>
+            </div>);
+        } else {
+            let myLibrary = "/" + this.props.userId;
+            return (<div>
+                <p className="messageContainer">
+                    <span>빌려줌 상태로 바뀌었습니다.</span>
+                </p>
+                <p className="selectContainer">
+                    <button className="button button_small" onClick={this.returnBook.bind(this)}>빌려줌 취소</button>
+                    <button className="button button_small"><Link to={myLibrary}>내도서관</Link></button>
+                </p>
+            </div>)
+        }
     }
 }
 
